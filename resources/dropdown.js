e4a4680c838ad9e51c52dca272707508189ddd6f
Keira Healey <healek@rpi.edu>
2025-06-09 21:16:57 -0400

    Refactor indents

--- resources/dropdown.js
+++ resources/dropdown.js
@@ -5,15 +5,16 @@ function openDropdown(dropdown) {
     let i;
     for (i = 0; i < dropdowns.length; i++) {
         const openDropdown = dropdowns[i];
-        if ( dropdowns[i] !== document.getElementById(dropdown) ) {
+        if (dropdowns[i] !== document.getElementById(dropdown)) {
             openDropdown.classList.remove("show");
         }
     }
     let dropdown_content = document.getElementById(dropdown);
     dropdown_content.classList.toggle("show");
 }
+
 // Close the dropdown if the user clicks in window.
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches(".dropbtn")) {
         const dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
